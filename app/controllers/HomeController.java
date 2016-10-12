89da46f61642c7eb467d993f6397764ae7aa8be7
Niels Boecker <nielsboecker@gmail.com>
2016-10-12 15:33:01 +0200

    Paste files again

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import de.htwg.minesweeper.aview.tui.TUI;
+import de.htwg.minesweeper.controller.*;
+import play.mvc.*;
+
+import play.mvc.Controller;
+import views.html.*;
+
+import static play.mvc.Results.ok;
+
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+
+        de.htwg.minesweeper.controller.Controller controller = new de.htwg.minesweeper.controller.Controller();
+        TUI tui = new TUI(controller);
+
+        String tuiOutput = tui.printTui();
+
+        return ok(tuiOutput);
+    }
+
+}
