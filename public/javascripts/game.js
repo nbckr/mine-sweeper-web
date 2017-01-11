60f7ffaeba24a859f72538f729df74ce8ddb96b5
Mark Unger <marunger@htwg-konstanz.de>
2017-01-11 21:34:03 +0100

    changed to localhost

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -49,7 +49,7 @@ $(function () {
 });
 
 function connectToWebSocket() {
-    webSocket = new WebSocket("wss://minesweeper-htwg.herokuapp.com/socket");
+    webSocket = new WebSocket("ws://localhost:9000/socket");
     logWebSocketConnection('Socket Status: ' + webSocket.readyState + ' (ready)');
 
     webSocket.onopen = function () {
